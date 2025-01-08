8a00586b636b208d3386761b6cf3260cfa1baae2
JingZheee <zhe03@graduate.utm.my>
2025-01-08 10:46:41 +0800

    Added theme

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,100 @@
+'use client';
+import { Box, Typography, Button, Paper, Stack, Container } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
 export default function Home() {
+  const theme = useTheme();
+
   return (
-    <main className="min-h-screen p-8">
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold mb-8">Welcome to Your App</h1>
-        <p className="text-lg">Start building your application here.</p>
-      </div>
-    </main>
+    <Container maxWidth="lg">
+      <Box sx={{ minHeight: '100vh', py: 4 }}>
+        {/* Theme Color Showcase */}
+        <Typography variant="h3" gutterBottom>
+          Theme Colors
+        </Typography>
+        
+        {/* Primary Colors */}
+        <Paper elevation={3} sx={{ p: 3, mb: 4 }}>
+          <Typography variant="h6" gutterBottom>
+            Primary Colors
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" color="primary">
+              Primary
+            </Button>
+            <Button variant="outlined" color="primary">
+              Primary Outlined
+            </Button>
+            <Button variant="text" color="primary">
+              Primary Text
+            </Button>
+          </Stack>
+        </Paper>
+
+        {/* Secondary Colors */}
+        <Paper elevation={3} sx={{ p: 3, mb: 4 }}>
+          <Typography variant="h6" gutterBottom>
+            Secondary Colors
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" color="secondary">
+              Secondary
+            </Button>
+            <Button variant="outlined" color="secondary">
+              Secondary Outlined
+            </Button>
+            <Button variant="text" color="secondary">
+              Secondary Text
+            </Button>
+          </Stack>
+        </Paper>
+
+        {/* Status Colors */}
+        <Paper elevation={3} sx={{ p: 3, mb: 4 }}>
+          <Typography variant="h6" gutterBottom>
+            Status Colors
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" color="success">
+              Success
+            </Button>
+            <Button variant="contained" color="error">
+              Error
+            </Button>
+            <Button variant="contained" color="warning">
+              Warning
+            </Button>
+            <Button variant="contained" color="info">
+              Info
+            </Button>
+          </Stack>
+        </Paper>
+
+        {/* Typography */}
+        <Paper elevation={3} sx={{ p: 3 }}>
+          <Typography variant="h6" gutterBottom>
+            Typography
+          </Typography>
+          <Typography variant="h1">h1. Heading</Typography>
+          <Typography variant="h2">h2. Heading</Typography>
+          <Typography variant="h3">h3. Heading</Typography>
+          <Typography variant="h4">h4. Heading</Typography>
+          <Typography variant="h5">h5. Heading</Typography>
+          <Typography variant="h6">h6. Heading</Typography>
+          <Typography variant="subtitle1">
+            subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </Typography>
+          <Typography variant="subtitle2">
+            subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </Typography>
+          <Typography variant="body1">
+            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </Typography>
+          <Typography variant="body2">
+            body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </Typography>
+        </Paper>
+      </Box>
+    </Container>
   );
 }
